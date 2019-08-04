28ddca3cb5c09235fba9fbcf67ae35d2ab6cb425
MatoSuraj <48233839+MatoSuraj@users.noreply.github.com>
2019-08-04 19:54:26 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { SearchService } from './search.service';
+import { SearchComponent } from './search/search.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [SearchService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
